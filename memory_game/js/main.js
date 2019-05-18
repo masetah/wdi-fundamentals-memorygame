1e1161eb21046a77caaa167dab73ed3a1a51a911
Mase Taherian <mase.taherian@assistwireless.com>
2019-05-18 12:42:16 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,21 +24,32 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
+	
 if (cardsInPlay[0] === cardsInPlay[1]) {
+  
   alert("You found a match!");
 } else {
   alert("Sorry, try again.");
 }	
 }
 
-function flipCard (cardId) {
+function flipCard () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].cardImage);
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay.length > 1) {
         checkForMatch();
     }
 }
-
-flipCard(0);
-flipCard(2);
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard()
